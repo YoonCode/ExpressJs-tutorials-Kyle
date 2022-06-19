f452741005277e5d086d4d0485243977ec8a3cf2
YoonCode <yjin830201@gmail.com>
2022-06-19 13:11:38 +0900

    you get sever error, but anyway send messages to the user

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express()
 app.get('/', (req, res) => {
   console.log('Here')
   // res.send('Hi')
-  res.sendStatus(500)
+  // res.sendStatus(500)
+  res.status(500).send('Helllo')
 })
 
 app.listen(3000)
