4a03344a81dfb5e11b368b3244c7128ede788030
YoonCode <yjin830201@gmail.com>
2022-06-19 15:27:09 +0900

    to practice how to use params.id

--- routes/products.js
+++ routes/products.js
@@ -9,4 +9,12 @@ router.get('/new', (req, res) => {
   res.send('Product New Form')
 })
 
+router.post('/', (req, res) => {
+  res.send('Create Product')
+})
+
+router.get('/:prodId', (req, res) => {
+  res.send(`Create Product With ID ${req.params.prodId}`)
+})
+
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,12 @@ router.get('/new', (req, res) => {
   res.send('User New Form')
 })
 
+router.post('/', (req, res) => {
+  res.send('Create User')
+})
+
+router.get('/:userId', (req, res) => {
+  res.send(`Create User With ID ${req.params.userId}`)
+})
+
 module.exports = router
