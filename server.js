7a90518e773ad5f62f4b308b85b0b82e05c0428a
YoonCode <yjin830201@gmail.com>
2022-06-19 13:31:21 +0900

    install ejs, ejs lang support extension, create views folder, and render index.ejs

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
   console.log('Here')
-  res.download('server.js')
+  // res.download('server.js')
+  res.render('index')
 })
 
 app.listen(3000)
