e10771c82a982277ca8c056d3deee1c34163a041
YoonCode <yjin830201@gmail.com>
2022-06-19 15:10:39 +0900

    push again

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   console.log('Here')
-  // res.download('server.js')
   res.render('index', { text: 'World' })
 })
+
+const userRouter = require('./routes/users')
+const productRouter = require('./routes/products')
+
+app.use('/users', userRouter)
+app.use('/products', productRouter)
 
 app.listen(3000)
