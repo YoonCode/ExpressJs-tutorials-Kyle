f5bfe1f8e28c198ff3571fb8f69fa5e51d1d830d
YoonCode <yjin830201@gmail.com>
2022-06-19 13:24:02 +0900

    you want user to download

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const app = express()
 
 app.get('/', (req, res) => {
   console.log('Here')
-  // res.send('Hi')
-  // res.sendStatus(500)
-  // res.status(500).send('Helllo')
-  res.status(500).json({ message: 'Error' })
+  res.download('server.js')
 })
 
 app.listen(3000)
